7267b0f8f16ee71a1a438b7cf3db7395d189cb2a
cerny-dan <168650261+cerny-dan@users.noreply.github.com>
2025-09-14 17:21:39 +0200

    Add files via upload

--- Slozka/main.py
+++ Slozka/main.py
@@ -3,5 +3,8 @@ b = int(input("Zadej číslo b: "))
 
 def secti(a, b):
     return a + b
-
 print(f"Součet je: ", secti(a, b))
+
+def rozdil(a, b):
+    return a - b
+print(f"Rozdíl je: ", rozdil(a, b))
