7434c63a709f41eb1e1dbc4b32ec0be4dddf2873
cerny-dan <168650261+cerny-dan@users.noreply.github.com>
2025-09-14 17:20:39 +0200

    Add files via upload

---
+++ notebook_verze/Slozka/main.py
@@ -0,0 +1,10 @@
+a = int(input("Zadej čáslo a: "))
+b = int(input("Zadej číslo b: "))
+
+def secti(a, b):
+    return a + b
+print(f"Součet je: ", secti(a, b))
+
+def rozdil(a, b):
+    return a - b
+print(f"Rozdíl je: ", rozdil(a, b))
